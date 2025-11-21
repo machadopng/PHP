21ad6b93234797220077851a2e2d323f289fc72e
machadopng <jorgemagalhaesmachado@gmail.com>
2025-11-21 15:12:37 -0300

    Add files via upload

--- Loterias.php
+++ Loterias.php
@@ -21,7 +21,7 @@ function menu(): void
         switch ($opcao) {
             case 1:
                 mega_sena();
-                $continuar = false;
+                sleep(2);
                 break;
             case 2:
                 quina();
@@ -44,39 +44,63 @@ function menu(): void
 
 function mega_sena(): void
 {
-    $sorteados = [];
+    do {
     $qntd_dezenas = (int)readline("Quantas dezenas deseja apostar (6-20)? ");
+    } while ($qntd_dezenas < 6 || $qntd_dezenas > 20);
+    
     $qntd_jogos = (int)readline("Quantos jogos deseja fazer? ");
-    $contador = 0;
-    while ($contador < $qntd_dezenas) {
-        $dezena_sorteada = rand(1, 60);
-        if (!in_array($dezena_sorteada, $sorteados)) {
-        $sorteados[] = $dezena_sorteada;
-        }
-        $contador++;
-    }
-    print_r($sorteados);
-
-    //sorteio
-    //calcular valores
+    sorteio_numeros($qntd_dezenas, 1, 60, $qntd_jogos, 6.0, 6);
+    
 }
-
 function quina(): void
 {
-    print "Você escolheu Quina!\n";
-    // Lógica específica para Quina
+    do {
+        $qntd_dezenas = (int)readline("Quantas dezenas deseja apostar (5-15)? ");
+    } while ($qntd_dezenas < 5 || $qntd_dezenas > 15);
+    $qntd_jogos = (int)readline("Quantos jogos deseja fazer? ");
+    sorteio_numeros($qntd_dezenas, 1, 80, $qntd_jogos, 3.0, 5);
 }
 
 function loto_facil(): void
 {
-    print "Você escolheu Lotofácil!\n";
-    // Lógica específica para Lotofácil
+    do {
+        $qntd_dezenas = (int)readline("Quantas dezenas deseja apostar (15-20)? ");
+    } while ($qntd_dezenas < 15 || $qntd_dezenas > 20);
+    $qntd_jogos = (int)readline("Quantos jogos deseja fazer? ");
+    sorteio_numeros($qntd_dezenas, 1, 25, $qntd_jogos, 3.50, 15);
 }
 
 function loto_mania(): void
 {
-    print "Você escolheu Lotomania!\n";
-    // Lógica específica para Lotomania
+    //trabalho do erik
 }
 
-function sorteio_numeros(): void {}
+function sorteio_numeros($dezenas, $min, $max, $jogos, $preço, $minajogar)
+{
+    for($i = 0; $i < $jogos; $i++)
+    {
+    $sorteados = [];
+    
+    while (sizeof($sorteados) < $dezenas) {
+        $dezena_sorteada = rand($min, $max);
+        if (!in_array($dezena_sorteada, $sorteados)) {
+        $sorteados[] = $dezena_sorteada;
+        }
+        
+    }
+    sort($sorteados);
+    
+    print(implode(" - ", $sorteados));
+    print "\n";
+    }
+    print("\nO preço total a ser gasto é: R$ " .  fatorial($dezenas) / (fatorial($minajogar) * fatorial($dezenas - $minajogar)) * $preço . "\n");
+ // (Fatorial das dezenas que voce escolhe / fatorial do minimo de dezenas quevoce PODE escolher * fatorial da diferenca de dezenas que voce escolhe pelo minimo) * preço   
+}
+function fatorial($num)
+{
+    $fat = 1;
+    for ($i = 1; $i <= $num; $i++) {
+        $fat *= $i;
+    }
+    return $fat;
+}
